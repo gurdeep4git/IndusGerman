5b0309ddd90d069d5dbe31b0a4a4992481f7dd34
gurdeep4git <g4gurdeep93@gmail.com>
2023-05-11 12:44:01 +0530

    updated banner

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -4,7 +4,20 @@ import './Banner.css';
 function Banner() {
     return (
         <>
-            <div className='banner'></div>
+            <div className='banner'>
+                <div className='blur'>
+                    <div className="container">
+                        <div className="row">
+                            <div className="col-lg-8 col-xl-6">
+                                <div className='margin wow fadeInDown' data-wow-delay="0.1s">
+                                    <h1 className="my-3 title">an ISO 13485:2016 &amp; ISO 9001:2015 Certified Company</h1>
+                                    <p className='text'>by CDSCO (Govt. Of India) notified body</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
